e33e2d617d03020e3594a40f6ddb49b78046092a
VaqifDadaszade <101572243+VaqifDadaszade@users.noreply.github.com>
2022-11-25 20:24:35 +0400

    Ders ile elaqedar kodlar.

--- src/vagif/dadaszade/SampleController.java
+++ src/vagif/dadaszade/SampleController.java
@@ -1,5 +1,20 @@
 package vagif.dadaszade;
 
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
+	@FXML
+	private TextField studentNameText;
 	
+	@FXML
+	private Label studentNameLable;
+	
+	@FXML
+	private void saveStudent() {
+		studentNameLable.setText("Salam Adiniz: "+studentNameText.getText());
+	}
 }
